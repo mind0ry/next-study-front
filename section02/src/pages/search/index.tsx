8473485980aba837e7c4c1becb76b22f6c56f395
mind0ry <mindory144@gmail.com>
2026-05-07 13:13:39 +0900

    11

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -3,14 +3,29 @@ import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import books from "@/mock/books.json"
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 
-export default function Page() {
+export const getServerSideProps = async (context : GetServerSidePropsContext) => {
 
-    return <div>
+    const q = context.query.q;
+    const books = await fetchBooks(q as string);
+
+    return { 
+        props: {},
+    };
+}
+
+export default function Page({
+    books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+    return (<div>
         {books.map((book) => (
             <BookItem key={book.id} {...book} />
         ))}
-    </div>;
+    </div>
+    );
 
 }
 
